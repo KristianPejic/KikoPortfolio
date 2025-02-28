dafc0932c2cf1fe303a4a96abef4715804c95b3e
Kristian Pejić <108726003+KristianPejic@users.noreply.github.com>
2025-02-28 17:05:34 +0100

    Loading screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'KikoPortfolio';
+export class AppComponent implements OnInit {
+  loading = true;
+  displayText = '';
+  private fullText = '<Loading>Welcome</Loading>';
+  private typingSpeed = 50;
+  private currentIndex = 0;
+
+  ngOnInit() {
+    this.typeText();
+
+    setTimeout(() => {
+      this.loading = false;
+    }, this.fullText.length * this.typingSpeed + 1000);
+  }
+
+  typeText() {
+    if (this.currentIndex < this.fullText.length) {
+      this.displayText += this.fullText.charAt(this.currentIndex);
+      this.currentIndex++;
+      setTimeout(() => this.typeText(), this.typingSpeed);
+    }
+  }
 }
