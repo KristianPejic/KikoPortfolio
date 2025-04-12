56a8d57efab09af987efd0b4cabc97b6ae1b1903
Kristian Pejić <108726003+KristianPejic@users.noreply.github.com>
2025-04-13 01:42:04 +0200

    Making the certificates to look like the education

--- src/app/components/certificates/certificates.component.ts
+++ src/app/components/certificates/certificates.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-certificates',
   templateUrl: './certificates.component.html',
+  styleUrls: ['./certificates.component.css'],
 })
 export class CertificatesComponent {
   certificates = [
